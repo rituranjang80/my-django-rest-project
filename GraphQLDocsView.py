ca444b47deb511e87560fb861603ab9ec64b0b64
rituranjan <rituranjang@audaxlabs.com>
2025-02-10 06:32:08 +0530

    test

--- GraphQLDocsView.py
+++ GraphQLDocsView.py
@@ -2,6 +2,7 @@ from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework.permissions import AllowAny
 
+
 class GraphQLDocsView(APIView):
     permission_classes = [AllowAny]
 
